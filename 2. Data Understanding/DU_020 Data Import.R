89865f020bb0c6833bb6af67f294c2154e35150d
wankata5 <ivanvrategov@gmail.com>
2018-09-29 10:15:53 +0300

    Import the data.

---
+++ 2. Data Understanding/DU_020 Data Import.R
@@ -0,0 +1,17 @@
+#######################
+#   load the data     #
+#######################
+datafile <- "E:/Datathon Sept/Air Tube/data_bg_2017.csv"
+df <- read_csv(datafile)
+
+
+
+
+
+
+
+
+
+
+
+
