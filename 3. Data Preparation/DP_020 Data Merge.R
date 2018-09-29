4a740c020baf9c56a69b3d627dc142f8be878cb3
Radi Dimitrov <radddi@abv.bg>
2018-09-29 20:16:05 +0300

    Data merge script WIP

--- 3. Data Preparation/DP_020 Data Merge.R
+++ 3. Data Preparation/DP_020 Data Merge.R
@@ -17,6 +17,8 @@ datafile2 <- "0. Data/Air Tube/data_bg_2018.csv"
 geohash_1 = "0. Data/geohash_mapping_sofia_2017_unique.csv"
 geohash_2 = "0. Data/geohash_mapping_sofia_2018_unique.csv"
 
+eea_file = "0. Data/EEA Data-20180928T184849Z-001/EEA Data/eea_final.csv"
+
 air_tube = rbind(
 	read.csv(file = datafile1),
 	read.csv(file = datafile2)
@@ -28,9 +30,24 @@ sofia_geohash = rbind(
 )
 sofia_geohash = unique(sofia_geohash)
 
+eea = read.csv(file = eea_file, stringsAsFactors = FALSE)
+
 #	Inner join air_tube to sofia geohash data
 data_analysis = merge(
 	air_tube,
 	sofia_geohash,
 	by = "geohash"
 )
+
+#	Fix EEA colnames
+eea_colnames = as.character(eea[1, ])
+colnames(eea)[-c(1:4)] = eea_colnames[-c(1:4)]
+eea = eea[-1, ]
+
+eea[, -c(1:4)] = sapply(
+	eea[, -c(1:4)],
+	as.numeric
+)
+
+eea = eea[, -1]
+
